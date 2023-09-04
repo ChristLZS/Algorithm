4763c08be2ad94560032a633686909431171538f
li zhisheng <zhishengli@deepglint.com>
2023-09-04 11:11:37 +0800

    [链表] 完成链表相交

--- 链表/链表相交/main.go
+++ 链表/链表相交/main.go
@@ -5,7 +5,33 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// 思路
+// 长度对齐，让长链表移动至对齐位置，然后两个链表同时前进
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
+
+	lenA, lenB := 0, 0
+	for node := headA; node != nil; node = node.Next {
+		lenA++
+	}
+	for node := headB; node != nil; node = node.Next {
+		lenB++
+	}
+	if lenA > lenB {
+		for i := 0; i < lenA-lenB; i++ {
+			headA = headA.Next
+		}
+	} else if lenA < lenB {
+		for i := 0; i < lenB-lenA; i++ {
+			headB = headB.Next
+		}
+	}
+	for headA != nil && headB != nil {
+		if headA == headB {
+			return headA
+		}
+		headA = headA.Next
+		headB = headB.Next
+	}
 	return nil
 }
 
