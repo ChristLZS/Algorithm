b28452d70317daa582e5d2fb955bddda66176f10
li zhisheng <zhishengli@deepglint.com>
2023-09-04 14:48:23 +0800

    [链表] 检测环型链表

--- 链表/环形链表II/main.go
+++ 链表/环形链表II/main.go
@@ -5,7 +5,24 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// 思路
+// 1.快慢指针，判断是否为环型
+// 2.如果是环型，快指针回到头节点，然后快慢指针同时前进，相遇的节点即为环的入口
 func detectCycle(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+
+		if fast == slow {
+			for head != slow {
+				head = head.Next
+				slow = slow.Next
+			}
+
+			return head
+		}
+	}
 	return nil
 }
 
