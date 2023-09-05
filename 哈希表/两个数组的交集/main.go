ea3346046b07cbc2a1a63f242a4b3b2013e8c2b7
li zhisheng <zhishengli@deepglint.com>
2023-09-05 19:24:39 +0800

    [哈希表] 两个数组的交集

--- 哈希表/两个数组的交集/main.go
+++ 哈希表/两个数组的交集/main.go
@@ -1,7 +1,23 @@
 package main
 
 func intersection(nums1 []int, nums2 []int) []int {
-	return nil
+	res := make(map[int]struct{}, 0)
+	tmp := make(map[int]struct{})
+	for _, v := range nums1 {
+		tmp[v] = struct{}{}
+	}
+	for _, v := range nums2 {
+		if _, ok := tmp[v]; ok {
+			res[v] = struct{}{}
+		}
+	}
+
+	result := make([]int, 0, len(res))
+	for k := range res {
+		result = append(result, k)
+	}
+
+	return result
 }
 
 func main() {
