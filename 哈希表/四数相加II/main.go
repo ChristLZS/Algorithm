c064361d5cb81a38f23361faa478dcbb1f3b27ea
li zhisheng <zhishengli@deepglint.com>
2023-09-06 16:39:56 +0800

    [哈希表] 四数相加

--- 哈希表/四数相加II/main.go
+++ 哈希表/四数相加II/main.go
@@ -1,7 +1,19 @@
 package main
 
 func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
-	return 0
+	m := make(map[int]int)
+	for _, v1 := range nums1 {
+		for _, v2 := range nums2 {
+			m[v1+v2]++
+		}
+	}
+	result := 0
+	for _, v3 := range nums3 {
+		for _, v4 := range nums4 {
+			result += m[-v3-v4]
+		}
+	}
+	return result
 }
 
 func main() {
