4d26b57012d33b9bd9eb83037513a50d5e6646fd
li zhisheng <zhishengli@deepglint.com>
2023-09-06 16:29:13 +0800

    [哈希表] 快乐数

--- 哈希表/快乐数/main.go
+++ 哈希表/快乐数/main.go
@@ -1,10 +1,30 @@
 package main
 
 func isHappy(n int) bool {
-	return false
+	m := make(map[int]struct{})
+	n = getSum(n)
+	for n != 1 {
+		if _, ok := m[n]; ok {
+			return false
+		} else {
+			m[n] = struct{}{}
+		}
+		n = getSum(n)
+	}
+
+	return true
+}
+
+func getSum(n int) int {
+	sum := 0
+	for t := n % 10; n != 0; t = n % 10 {
+		sum += t * t
+		n /= 10
+	}
+	return sum
 }
 
 func main() {
-	result := isHappy(19)
+	result := isHappy(2)
 	println(result)
 }
