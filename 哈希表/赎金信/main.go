41a7d24a0801fc9fcfd897dec69cc6e0804707e8
li zhisheng <zhishengli@deepglint.com>
2023-09-06 16:40:38 +0800

    [哈希表] 赎金信

--- 哈希表/赎金信/main.go
+++ 哈希表/赎金信/main.go
@@ -1,10 +1,21 @@
 package main
 
 func canConstruct(ransomNote string, magazine string) bool {
-	return false
+	m := make(map[rune]int)
+	for _, v := range magazine {
+		m[v]++
+	}
+	for _, v := range ransomNote {
+		if m[v] > 0 {
+			m[v]--
+		} else {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-	result := canConstruct("a", "b")
+	result := canConstruct("ab", "ba")
 	println(result)
 }
