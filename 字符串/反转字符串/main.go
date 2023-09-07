6e1777c29cbe5b9ed8c589b984d7c276b6f7a2f6
li zhisheng <zhishengli@deepglint.com>
2023-09-07 18:38:36 +0800

    [字符串] 反转字符转

--- 字符串/反转字符串/main.go
+++ 字符串/反转字符串/main.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 func reverseString(s []byte) {
-
+	var left, right = 0, len(s) - 1
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
 }
 
 func main() {
 	s := []byte{'h', 'e', 'l', 'l', 'o'}
 	reverseString(s)
-	fmt.Println(s)
+	fmt.Println(string(s))
 }
