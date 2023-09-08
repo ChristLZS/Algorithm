dc1e854de2f25b0654a3430f687a0ff6e99695e7
li zhisheng <zhishengli@deepglint.com>
2023-09-08 14:56:22 +0800

    [字符串] 反转字符串II

--- 字符串/反转字符串II/main.go
+++ 字符串/反转字符串II/main.go
@@ -3,7 +3,24 @@ package main
 import "fmt"
 
 func reverseStr(s string, k int) string {
-	return ""
+	res := []byte(s)
+	for i := 0; i < len(res); i += 2 * k {
+		if i+k <= len(res) {
+			reverseString(res[i : i+k])
+		} else {
+			reverseString(res[i:])
+		}
+	}
+	return string(res)
+}
+
+func reverseString(s []byte) {
+	var left, right = 0, len(s) - 1
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
 }
 
 func main() {
