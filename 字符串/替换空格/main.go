1be25e76488ca7194967de1c8aa2b20aeeeea24d
li zhisheng <zhishengli@deepglint.com>
2023-09-08 15:21:48 +0800

    [字符串] 替换空格

--- 字符串/替换空格/main.go
+++ 字符串/替换空格/main.go
@@ -2,8 +2,33 @@ package main
 
 import "fmt"
 
+// 做到极致，不用库函数和额外空间
 func replaceSpace(s string) string {
-	return ""
+	count := 0
+	for _, v := range s {
+		if v == ' ' {
+			count++
+		}
+	}
+
+	res := make([]byte, 0, len(s)+count*2)
+	res = append(res, s...)
+	res = append(res, make([]byte, count*2)...)
+	left := len(s) - 1
+	for right := len(res) - 1; right >= 0; {
+		if res[left] == ' ' {
+			res[right] = '0'
+			res[right-1] = '2'
+			res[right-2] = '%'
+			right -= 3
+		} else {
+			res[right] = res[left]
+			right--
+		}
+		left--
+	}
+
+	return string(res)
 }
 
 func main() {
