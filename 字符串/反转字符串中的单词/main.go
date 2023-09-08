8775b8b39fdb0edf16367e1921d3d1aa9b047f52
li zhisheng <zhishengli@deepglint.com>
2023-09-08 16:55:50 +0800

    [字符串] 反转字符串中的单词

--- 字符串/反转字符串中的单词/main.go
+++ 字符串/反转字符串中的单词/main.go
@@ -3,9 +3,69 @@ package main
 import "fmt"
 
 func reverseWords(s string) string {
-	return ""
+	data := []byte(s)
+	// 去除首尾空格
+	for i := 0; i < len(data); i++ {
+		if data[i] != ' ' {
+			data = data[i:]
+			break
+		}
+	}
+	for i := len(data) - 1; i >= 0; i-- {
+		if data[i] != ' ' {
+			data = data[:i+1]
+			break
+		}
+	}
+
+	// 去除中间多余空格
+	var count = 0
+	var left, right = 0, 0
+	for right < len(data) {
+		if data[right] == ' ' {
+			data[left] = ' '
+			left++
+			right++
+			for data[right] == ' ' {
+				right++
+				count++
+			}
+		} else {
+			data[left] = data[right]
+			left++
+			right++
+		}
+	}
+	data = data[:len(data)-count]
+
+	// 整体反转
+	reverseString(data)
+
+	// 单词反转
+	left, right = 0, 0
+	for right < len(data) {
+		if data[right] == ' ' {
+			reverseString(data[left:right])
+			left = right + 1
+		}
+		right++
+	}
+
+	// 最后一个单词
+	reverseString(data[left:])
+
+	return string(data)
+}
+
+func reverseString(s []byte) {
+	var left, right = 0, len(s) - 1
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
 }
 
 func main() {
-	fmt.Println(reverseWords("the sky is blue"))
+	fmt.Println(reverseWords("a good   example"))
 }
