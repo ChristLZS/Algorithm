6fb5fc075292ea60dc731a8336b5869738eb3226
li zhisheng <zhishengli@deepglint.com>
2023-09-13 14:55:31 +0800

    [双指针] 移除元素 && 反转字符串

--- 双指针/反转字符串/main.go
+++ 双指针/反转字符串/main.go
@@ -1,11 +1,16 @@
 package main
 
 func reverseString(s []byte) {
-
+	var left, right = 0, len(s) - 1
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
 }
 
 func main() {
 	s := []byte{'h', 'e', 'l', 'l', 'o'}
 	reverseString(s)
-	println(s)
+	println(string(s))
 }
--- 双指针/移除元素/main.go
+++ 双指针/移除元素/main.go
@@ -1,7 +1,15 @@
 package main
 
 func removeElement(nums []int, val int) int {
-	return 0
+	var slow, fast = 0, 0
+	for fast < len(nums) {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
 }
 
 func main() {
