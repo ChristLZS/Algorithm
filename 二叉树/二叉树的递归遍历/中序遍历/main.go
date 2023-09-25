3a85908758a5d1bab4d2d4b539386ca0a5280859
li zhisheng <zhishengli@deepglint.com>
2023-09-25 17:13:27 +0800

    [二叉树] 递归遍历

--- 二叉树/二叉树的递归遍历/中序遍历/main.go
+++ 二叉树/二叉树的递归遍历/中序遍历/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func visit(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	res := make([]int, 0)
+	// 左子树
+	res = append(res, visit(root.Left)...)
+
+	// 根节点
+	res = append(res, root.Val)
+
+	// 右子树
+	res = append(res, visit(root.Right)...)
+
+	return res
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	return visit(root)
+}
+
+func main() {
+	root := &TreeNode{Val: 1}
+	root.Right = &TreeNode{Val: 2}
+	root.Right.Left = &TreeNode{Val: 3}
+	inorderTraversal(root)
+}
--- 二叉树/二叉树的递归遍历/后序遍历/main.go
+++ 二叉树/二叉树的递归遍历/后序遍历/main.go
@@ -1,3 +1,32 @@
 package main
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func visit(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	res := make([]int, 0)
+
+	// 左子树
+	res = append(res, visit(root.Left)...)
+
+	// 右子树
+	res = append(res, visit(root.Right)...)
+
+	// 根节点
+	res = append(res, root.Val)
+
+	return res
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	return visit(root)
+}
+
 func main() {}
