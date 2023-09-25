094e93835f64abb1073c19e38bf4b1eff74d5212
li zhisheng <zhishengli@deepglint.com>
2023-09-25 19:08:49 +0800

    [二叉树] 迭代法,前序遍历

--- 二叉树/二叉树的迭代遍历/前序遍历/main.go
+++ 二叉树/二叉树的迭代遍历/前序遍历/main.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	if nil == root {
+		return nil
+	}
+
+	res := make([]int, 0)
+	stack := make([]*TreeNode, 0)
+	stack = append(stack, root)
+	for len(stack) > 0 {
+		// 添加根节点
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, node.Val)
+
+		// 添加右节点
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+		}
+
+		// 添加左节点
+		if node.Left != nil {
+			stack = append(stack, node.Left)
+		}
+	}
+
+	return res
+}
+
+func main() {
+	fmt.Println(preorderTraversal(&TreeNode{Val: 1, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 3}}}))
+
+}
