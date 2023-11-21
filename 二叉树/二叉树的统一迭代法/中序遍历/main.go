39fa67476574eca9648dd0982cc83a85b0442573
li zhisheng <zhishengli@deepglint.com>
2023-11-21 14:37:20 +0800

    [二叉树的统一遍历法] 前、中\后

--- 二叉树/二叉树的统一迭代法/中序遍历/main.go
+++ 二叉树/二叉树的统一迭代法/中序遍历/main.go
@@ -1,3 +1,48 @@
 package main
 
-func main() {}
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	stack := make([]*TreeNode, 0)
+	res := make([]int, 0)
+	cur := root
+	stack = append(stack, cur)
+	for len(stack) > 0 {
+		cur = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if cur != nil { // 添加节点
+			if cur.Right != nil {
+				stack = append(stack, cur.Right)
+			}
+
+			// 标记节点
+			stack = append(stack, cur)
+			stack = append(stack, nil)
+
+			if cur.Left != nil {
+				stack = append(stack, cur.Left)
+			}
+
+		} else { // 处理节点
+			cur = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res = append(res, cur.Val)
+		}
+	}
+
+	return res
+}
+
+func main() {
+	fmt.Println(preorderTraversal(&TreeNode{Val: 1, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 3}}}))
+}
--- 二叉树/二叉树的统一迭代法/前序遍历/main.go
+++ 二叉树/二叉树的统一迭代法/前序遍历/main.go
@@ -1,3 +1,45 @@
 package main
 
-func main() {}
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 统一迭代法
+// 前序遍历：根左右
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	res := make([]int, 0)
+	stack := make([]*TreeNode, 0)
+	cur := root
+	stack = append(stack, cur)
+	for len(stack) > 0 {
+		// 处理一个节点
+		cur = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if cur != nil { // 添加节点
+			if cur.Right != nil {
+				stack = append(stack, cur.Right)
+			}
+			if cur.Left != nil {
+				stack = append(stack, cur.Left)
+			}
+			stack = append(stack, cur)
+			stack = append(stack, nil) // 标记节点
+		} else { // 处理节点
+			cur = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res = append(res, cur.Val)
+		}
+	}
+
+	return res
+}
+
+func main() {
+	preorderTraversal(&TreeNode{Val: 1, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 3}}})
+}
