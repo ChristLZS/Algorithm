da7f23ce5bc1ce5a7c56cc33b17bc05917b13f8b
li zhisheng <zhishengli@deepglint.com>
2023-11-22 11:29:24 +0800

    [二叉树] 二叉树的层序遍历||

--- 二叉树/二叉树层序遍历/二叉树的层序遍历II/main.go
+++ 二叉树/二叉树层序遍历/二叉树的层序遍历II/main.go
@@ -1,3 +1,49 @@
 package main
 
-func main() {}
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrderBottom(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+
+	res := make([][]int, 0)
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	// 每次处理一层
+	for len(queue) > 0 {
+		tmpRes := make([]int, 0)
+		tmpQueue := make([]*TreeNode, 0)
+		for len(queue) > 0 {
+			cur := queue[0]
+			queue = queue[1:]
+			if cur.Left != nil {
+				tmpQueue = append(tmpQueue, cur.Left)
+			}
+			if cur.Right != nil {
+				tmpQueue = append(tmpQueue, cur.Right)
+			}
+			tmpRes = append(tmpRes, cur.Val)
+		}
+
+		res = append(res, tmpRes)
+		queue = tmpQueue
+	}
+
+	// 反转
+	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
+		res[i], res[j] = res[j], res[i]
+	}
+
+	return res
+}
+
+func main() {
+	fmt.Println(levelOrderBottom(&TreeNode{Val: 1, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 3}}}))
+}
