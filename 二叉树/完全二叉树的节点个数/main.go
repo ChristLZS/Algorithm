5949c901b01558db2bddead1a60e91099b2e7228
li zhisheng <zhishengli@deepglint.com>
2023-12-02 13:37:15 +0800

    [二叉树] 完全二叉树的节点数

--- 二叉树/完全二叉树的节点个数/main.go
+++ 二叉树/完全二叉树的节点个数/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func countNodes(root *TreeNode) int {
+	if nil == root {
+		return 0
+	}
+
+	res := 1
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		tmpQueue := []*TreeNode{}
+		for _, node := range queue {
+			res++
+			if node.Left != nil {
+				tmpQueue = append(tmpQueue, node.Left)
+			}
+			if node.Right != nil {
+				tmpQueue = append(tmpQueue, node.Right)
+			}
+		}
+		queue = tmpQueue
+	}
+
+	return res
+}
+
+func main() {
+
+}
