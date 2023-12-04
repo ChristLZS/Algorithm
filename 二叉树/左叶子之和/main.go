ab1d30a7c86128997cd99e08a2f9d2ab9339491e
li zhisheng <zhishengli@deepglint.com>
2023-12-04 19:34:40 +0800

    [二叉树] 左叶子节点之和

--- 二叉树/左叶子之和/main.go
+++ 二叉树/左叶子之和/main.go
@@ -1,3 +1,44 @@
 package main
 
-func main() {}
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 层序遍历
+func sumOfLeftLeaves(root *TreeNode) int {
+	if nil == root {
+		return 0
+	}
+
+	res := 0
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		tmpQueue := []*TreeNode{}
+		for _, node := range queue {
+			if left := node.Left; left != nil {
+				// 判断是否为叶子节点
+				if left.Left == nil && left.Right == nil {
+					res += left.Val
+				}
+
+				tmpQueue = append(tmpQueue, left)
+			}
+
+			if node.Right != nil {
+				tmpQueue = append(tmpQueue, node.Right)
+			}
+		}
+
+		queue = tmpQueue
+	}
+
+	return res
+}
+
+func main() {
+	fmt.Println(sumOfLeftLeaves(&TreeNode{3, &TreeNode{9, nil, nil}, &TreeNode{20, &TreeNode{15, nil, nil}, &TreeNode{7, nil, nil}}}))
+}
