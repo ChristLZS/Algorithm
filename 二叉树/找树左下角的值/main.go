39c61a6a7be5f05facbe317aafafb3903ed6ac83
li zhisheng <zhishengli@deepglint.com>
2023-12-05 17:41:13 +0800

    [二叉树] 找树左下角的值

--- 二叉树/找树左下角的值/main.go
+++ 二叉树/找树左下角的值/main.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findBottomLeftValue(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	res := root.Val
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		tmpQueue := []*TreeNode{}
+		for _, node := range queue {
+			if node.Left != nil {
+				tmpQueue = append(tmpQueue, node.Left)
+			}
+
+			if node.Right != nil {
+				tmpQueue = append(tmpQueue, node.Right)
+			}
+		}
+
+		if len(tmpQueue) > 0 {
+			res = tmpQueue[0].Val
+		}
+
+		queue = tmpQueue
+	}
+
+	return res
+}
+
+func main() {
+	fmt.Println(findBottomLeftValue(&TreeNode{3, &TreeNode{9, nil, nil}, &TreeNode{20, &TreeNode{15, nil, nil}, &TreeNode{7, nil, nil}}}))
+}
