9fd4461e6d22a6f451b7824020faa48bd1eac396
li zhisheng <zhishengli@deepglint.com>
2023-12-10 00:07:05 +0800

    [二叉树] 构造二叉树

--- 二叉树/最大二叉树/main.go
+++ 二叉树/最大二叉树/main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+	if len(nums) == 0 { // 递归终止条件
+		return nil
+	}
+
+	max := 0
+	index := -1
+	for i, v := range nums {
+		if v > nums[max] {
+			max = i
+			index = i
+		}
+	}
+
+	return &TreeNode{
+		Val:   max,
+		Left:  constructMaximumBinaryTree(nums[:index]),
+		Right: constructMaximumBinaryTree(nums[index+1:]),
+	}
+}
+
+func main() {
+	fmt.Println(constructMaximumBinaryTree([]int{3, 2, 1, 6, 0, 5}))
+}
