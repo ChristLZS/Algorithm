0a52df4e0566efadf1fa56aa2f68428d9ba34664
li zhisheng <zhishengli@deepglint.com>
2023-12-25 17:19:05 +0800

    [二叉树] 二叉树的最近公共祖先

--- 二叉树/二叉搜索树中的众数/main.go
+++ 二叉树/二叉搜索树中的众数/main.go
@@ -1,3 +1,52 @@
 package main
 
-func main() {}
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findMode(root *TreeNode) []int {
+	res := []int{}
+
+	var pre *TreeNode
+
+	var maxCount, count int
+
+	// 中序遍历
+	var vist = func(node *TreeNode) {}
+	vist = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		vist(node.Left)
+
+		if pre != nil && pre.Val == node.Val {
+			count++
+		} else {
+			count = 1
+		}
+
+		if count > maxCount {
+			maxCount = count
+			res = []int{node.Val}
+		} else if count == maxCount {
+			res = append(res, node.Val)
+		}
+
+		pre = node
+
+		vist(node.Right)
+	}
+
+	vist(root)
+	return res
+}
+
+func main() {
+	// [1,null,2,2]
+	fmt.Println(findMode(&TreeNode{Val: 1, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 2}}}))
+}
