42e02523af5b6e53451369f0b15e05f53bea81ee
li zhisheng <zhishengli@deepglint.com>
2024-01-02 23:37:51 +0800

    [二叉树] 二叉搜索树的插入操作

--- 二叉树/二叉搜索树中的插入操作/main.go
+++ 二叉树/二叉搜索树中的插入操作/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if nil == root {
+		return &TreeNode{Val: val}
+	}
+
+	var vist = func(node *TreeNode) {}
+	vist = func(node *TreeNode) {
+		if val > node.Val {
+			if node.Right == nil {
+				node.Right = &TreeNode{Val: val}
+			} else {
+				vist(node.Right)
+			}
+		} else {
+			if node.Left == nil {
+				node.Left = &TreeNode{Val: val}
+			} else {
+				vist(node.Left)
+			}
+		}
+	}
+
+	vist(root)
+
+	return root
+}
+
+func main() {
+	fmt.Println(insertIntoBST(nil, 0))
+}
