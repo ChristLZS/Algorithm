b6121dcfd04d804c1a10d42013c39d8795bb7c07
li zhisheng <zhishengli@deepglint.com>
2024-01-04 11:03:43 +0800

    [二叉树] 修剪二叉搜索树

--- 二叉树/修剪二叉搜索树/main.go
+++ 二叉树/修剪二叉搜索树/main.go
@@ -1,3 +1,39 @@
 package main
 
-func main() {}
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	var visit func(node *TreeNode) *TreeNode
+	visit = func(node *TreeNode) *TreeNode {
+		if node == nil {
+			return nil
+		}
+
+		if node.Val < low {
+			return visit(node.Right)
+		} else if node.Val > high {
+			return visit(node.Left)
+		}
+
+		node.Left = visit(node.Left)
+		node.Right = visit(node.Right)
+
+		return node
+	}
+
+	return visit(root)
+}
+
+func main() {
+	fmt.Println(trimBST(nil, 0, 0))
+}
