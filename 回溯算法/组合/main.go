af0e01991756bef5e745b6960205d4625e3ae59f
li zhisheng <zhishengli@deepglint.com>
2024-01-06 14:13:57 +0800

    [组合] 组合问题

--- 回溯算法/组合/main.go
+++ 回溯算法/组合/main.go
@@ -1,7 +1,32 @@
 package main
 
+import "fmt"
+
 func combine(n int, k int) [][]int {
-	return nil
+	res := [][]int{}
+
+	var dfs func(int, []int)
+	dfs = func(index int, curRes []int) {
+		// 回溯退出条件
+		if len(curRes) == k {
+			res = append(res, append([]int{}, curRes...))
+			return
+		}
+
+		// 递归
+		for i := index; i <= n; i++ {
+			curRes = append(curRes, i)
+			dfs(i+1, curRes)
+			curRes = curRes[:len(curRes)-1]
+		}
+	}
+
+	dfs(1, []int{})
+
+	return res
 }
 
-func main() {}
+func main() {
+	// 4 2
+	fmt.Println(combine(4, 2))
+}
