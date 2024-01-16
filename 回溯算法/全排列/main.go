577c4e56dd422e58fa0376cfebbbe137f77962d1
li zhisheng <zhishengli@deepglint.com>
2024-01-16 11:48:44 +0800

    [组合] 全排列

--- 回溯算法/全排列/main.go
+++ 回溯算法/全排列/main.go
@@ -1,3 +1,39 @@
 package main
 
-func main() {}
+import "fmt"
+
+func permute(nums []int) [][]int {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	res := [][]int{}
+	used := make(map[int]bool)
+	var dfs func([]int)
+	dfs = func(tmpRes []int) {
+		// 递归退出条件
+		if len(tmpRes) == len(nums) {
+			res = append(res, append([]int{}, tmpRes...))
+			return
+		}
+
+		// 递归
+		for i := 0; i < len(nums); i++ { // 全排列，不需要剪枝
+			if !used[i] {
+				used[i] = true
+				tmpRes = append(tmpRes, nums[i])
+				dfs(tmpRes)
+				tmpRes = tmpRes[:len(tmpRes)-1]
+				used[i] = false
+			}
+		}
+	}
+
+	dfs([]int{})
+
+	return res
+}
+
+func main() {
+	fmt.Println(permute([]int{1, 2, 3}))
+}
--- 回溯算法/全排列II/main.go
+++ 回溯算法/全排列II/main.go
@@ -1,3 +1,48 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"sort"
+)
+
+func permuteUnique(nums []int) [][]int {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	res := [][]int{}
+	sort.Ints(nums)
+	used := make([]int, len(nums)) //用于去重
+	var dfs func([]int)
+	dfs = func(tmpRes []int) {
+		// 递归退出条件
+		if len(tmpRes) == len(nums) {
+			res = append(res, append([]int{}, tmpRes...))
+			return
+		}
+
+		// 递归
+		for i := 0; i < len(nums); i++ {
+			// 去重：同一层递归中，如果前一个元素和当前元素相同，且前一个元素已经被使用过了，那么当前元素就不能使用
+			if i > 0 && nums[i] == nums[i-1] && used[i-1] == 0 {
+				continue
+			}
+
+			if used[i] == 0 { // == 0 表示没有被使用过
+				used[i] = 1
+				tmpRes = append(tmpRes, nums[i])
+				dfs(tmpRes)
+				tmpRes = tmpRes[:len(tmpRes)-1]
+				used[i] = 0
+			}
+		}
+	}
+
+	dfs([]int{})
+
+	return res
+}
+
+func main() {
+	fmt.Println(permuteUnique([]int{1, 1, 2}))
+}
