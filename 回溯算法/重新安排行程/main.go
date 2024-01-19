1ea96c59ebf5720638e8593357bc14aab609cca5
li zhisheng <zhishengli@deepglint.com>
2024-01-20 00:21:55 +0800

    [回溯] 全排列II

--- 回溯算法/重新安排行程/main.go
+++ 回溯算法/重新安排行程/main.go
@@ -1,3 +1,12 @@
 package main
 
-func main() {}
+import "fmt"
+
+func findItinerary(tickets [][]string) []string {
+
+	return nil
+}
+
+func main() {
+	fmt.Println(findItinerary([][]string{{"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"}}))
+}
