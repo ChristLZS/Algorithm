ad25d2c5c25ff0731f4e9c155e5ee6136bb5ca30
li zhisheng <zhishengli@deepglint.com>
2024-01-23 22:51:15 +0800

    [贪心] 摆动序列

--- 贪心算法/摆动序列/main.go
+++ 贪心算法/摆动序列/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import "fmt"
+
+func wiggleMaxLength(nums []int) int {
+	// 一个元素
+	if len(nums) < 2 {
+		return len(nums)
+	}
+
+	// 两个元素
+	if len(nums) == 2 {
+		if nums[0] == nums[1] {
+			return 1
+		} else {
+			return 2
+		}
+	}
+
+	// 遍历数组
+	var res = 1
+	var preDiff = 0
+	var curDiff = 0
+	for i := 0; i < len(nums)-1; i++ {
+		// 当前差值
+		curDiff = nums[i+1] - nums[i]
+
+		// 如果当前差值和上一个差值异号，那么就是一个峰值
+		if preDiff >= 0 && curDiff < 0 || preDiff <= 0 && curDiff > 0 {
+			res++
+			preDiff = curDiff
+		}
+	}
+
+	return res
+}
+
+func main() {
+	fmt.Println(wiggleMaxLength([]int{1, 7, 4, 9, 2, 5}))
+}
