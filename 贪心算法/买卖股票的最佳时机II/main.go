c493658e0b3a4246bca617fdf26ba6d03638a6b4
li zhisheng <zhishengli@deepglint.com>
2024-01-27 22:00:55 +0800

    [贪心] 买卖股票的最佳实际

--- 贪心算法/买卖股票的最佳时机II/main.go
+++ 贪心算法/买卖股票的最佳时机II/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import "fmt"
+
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+
+	// 贪心算法
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		// 只要今天的价格比昨天的价格高，就可以卖出
+		if prices[i] > prices[i-1] {
+			maxProfit += prices[i] - prices[i-1]
+		}
+	}
+
+	return maxProfit
+}
+
+func main() {
+	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+}
